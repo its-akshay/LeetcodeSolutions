89eaaf31d62466c74054ae1a5efc8000f28e0afa
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2023-07-29 01:02:33 +0530

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -9,6 +9,7 @@ using namespace std;
 class Solution {
   public:
     void topoSort(int node,vector<pair<int,int>>adj[],int vis[],stack<int>&st){
+        
         vis[node] = 1;
         for(auto it:adj[node]){
             int v = it.first;
@@ -16,10 +17,11 @@ class Solution {
                topoSort(v,adj,vis,st);
             }
         }
+        
         st.push(node);
     }
      vector<int> shortestPath(int N,int M, vector<vector<int>>& edges){
-         //1 creation of graph in the form of list
+         
         vector<pair<int,int>>adj[N];
         for(int i=0;i<M;i++){
             int u = edges[i][0];
@@ -27,6 +29,7 @@ class Solution {
             int wt = edges[i][2];
             adj[u].push_back({v,wt});
         }
+        
         int vis[N] = {0};
         stack<int>st;
         for(int i=0;i<N;i++){
@@ -34,10 +37,12 @@ class Solution {
                 topoSort(i,adj,vis,st);
             }
         }
+        
         vector<int>dist(N);
         for(int i=0;i<N;i++){
             dist[i] = 1e9;
         }
+        
         dist[0] = 0;
         while(!st.empty()){
             int node = st.top();
@@ -50,8 +55,12 @@ class Solution {
                 }
             }
         }
+        
         for (int i = 0; i < N; i++) {
-        if (dist[i] == 1e9) dist[i] = -1;
+        if (dist[i] == 1e9) 
+        {
+            dist[i] = -1;
+        }
       }
       return dist;
     }
