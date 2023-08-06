82060ca120ac879e888fbbc8b4a461d626dfb9d5
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2023-08-06 16:12:58 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (27.49%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int count  = 0;
+    int totalNQueens(int n) {
+        vector<string>ans(n);
+        vector<vector<string>>res;
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            ans[i] = s;
+        }
+        //till this moment i created a vector o strings
+        vector<int>left(n,0);
+        vector<int>upper(2*n-1,0);
+        vector<int>lower(2*n-1,0);
+        solve(0,ans,res,left,upper,lower,n);
+        return count;
+    }
+    void solve(int col,vector<string>&ans,vector<vector<string>>&res,vector<int>&left,vector<int>&upper,vector<int>&lower,int n){
+        if(col==n){
+            res.push_back(ans);
+            count++;
+            return;
+        }
+        for(int row=0;row<n;row++){
+            if(left[row]==0&&lower[row+col]==0&&upper[n-1+row-col]==0){
+                ans[row][col]='Q';
+                left[row] = 1;
+                lower[row+col] = 1;
+                upper[n-1+row-col]=1;
+                solve(col+1,ans,res,left,upper,lower,n);
+                 ans[row][col]='.';
+                left[row] = 0;
+                lower[row+col] = 0;
+                upper[n-1+row-col]=0;
+            }
+        }
+    }
+        
+};
