3579a9de31bf5587a0e5edb898eb0d510a2737ef
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2023-08-18 20:00:04 +0530

    Time: 3 ms (93.87%), Space: 10 MB (81.79%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.cpp
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
 
-    unordered_set<string> res;
 
-    void fun(int i,string s){
+    void fun(int i,string s,vector<string>&v){
         if(i == s.size()){
-            res.insert(s);
+            v.push_back(s);
             return;
         }
 
-        fun(i+1,s);
+        fun(i+1,s,v);
 
-        if('a' <= s[i] <= 'z' || 'A' <= s[i] <= 'Z'){
+        if (('a' <= s[i] && s[i] <= 'z') || ('A' <= s[i] && s[i] <= 'Z')) {
             if(islower(s[i])){
                 s[i] = toupper(s[i]);
             }
             else{
                 s[i] = tolower(s[i]);
             }
-            fun(i+1,s);
+            fun(i+1,s,v);
         }
     }
 
     vector<string> letterCasePermutation(string s) {
-        fun(0,s);
-        vector<string> ans;
-        for(auto it:res) ans.push_back(it);
-        return ans;
+        vector<string>v;
+        fun(0,s,v);
+        return v;
     }
 };
