5e7e87167026098b4de59e6cd74985a2b9fda6aa
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2023-08-28 21:27:46 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (95.85%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-    int sol(vector<int>&nums){
-        int prev = nums[0];
+     int robHelper(vector<int>& nums, int start, int end) {
+        int prev = 0;
         int prev2 = 0;
-        for(int i=1;i<nums.size();i++){
-            int pick = nums[i];
-            if(i>1){
-                pick+=prev2;
-            }
-            int notPick = 0 + prev;
-            int curr = max(pick,notPick);
+        
+        for (int i = start; i <= end; i++) {
+            int pick = nums[i] + prev2;
+            int notPick = prev;
+            int curr = max(pick, notPick);
             prev2 = prev;
             prev = curr;
         }
+        
         return prev;
     }
     int rob(vector<int>& nums) {
-        vector<int>temp1;
-        vector<int>temp2;
-        if(nums.size()==1){
+        int n = nums.size();
+        
+        if (n == 1) {
             return nums[0];
         }
-        for(int i=0;i<nums.size();i++){
-            if(i!=0){
-                temp1.push_back(nums[i]);
-            }
-            if(i!=nums.size()-1){
-                temp2.push_back(nums[i]);
-            }
-        }
-        return max(sol(temp1),sol(temp2));
+        
+        return max(robHelper(nums, 0, n - 2), robHelper(nums, 1, n - 1));
     }
 };
