2a37dc0fdb72dc2cca61f25b508040bca4701937
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2023-08-28 23:18:27 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (47.58%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
-    int solve(int ind,string &s,vector<int>&dp){
-        if(ind==0||ind==-1){
-            return 1;
-        }
-        if(ind<-1){
+    int numDecodings(string s) {
+        int n = s.length();
+        
+        if (n == 0 || s[0] == '0') {
             return 0;
         }
-        if(dp[ind]!=-1){
-            return dp[ind];
-        }
-        int count = 0;
-        if(s[ind]!='0'){
-            count = solve(ind-1,s,dp);
-        }
-        int num = (s[ind-1]-'0')*10+(s[ind]-'0');
-        if(ind>0){
-            if(num<=26&&num>=10){
-                count+=solve(ind-2,s,dp);
+        
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; ++i) {
+            // Count single-digit decoding
+            if (s[i - 1] != '0') {
+                dp[i] += dp[i - 1];
+            }
+            
+            // Count two-digit decoding
+            int num = (s[i - 2] - '0') * 10 + (s[i - 1] - '0');
+            if (num >= 10 && num <= 26) {
+                dp[i] += dp[i - 2];
             }
         }
-        dp[ind] = count;
-        return count;
-    }
-    int numDecodings(string s) {
-        if(s[0]=='0'){
-            return 0;
-            
-        }
-        int n = s.length();
-        vector<int>dp(n+1,-1);
-        return solve(n-1,s,dp);
+        
+        return dp[n];
     }
 };
