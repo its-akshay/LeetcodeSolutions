ab75a7768a3c84ee10962a4432e6ea93747f687e
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2023-08-28 12:33:39 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (40.70%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -4,10 +4,13 @@ public:
         vector<int> dp(n+1,-1);
         //we are here finding the distinct number of solutions actually
         //so here by assigning the value as 1 we mean that 
+        if(n==0||n==1){
+            return 1;
+        }
+        dp[2] = 2;
         dp[1] = 1;
-        dp[0] = 1;
         
-        for(int i=2;i<=n;i++){
+        for(int i=3;i<=n;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
         return dp[n];
