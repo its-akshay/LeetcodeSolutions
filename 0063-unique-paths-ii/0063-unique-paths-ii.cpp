5423a2ba1e197b2604fd9154a7fd5b372c95d481
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2023-08-31 17:30:46 +0530

    Time: 5 ms (37.56%), Space: 7.8 MB (34.04%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -12,11 +12,11 @@ public:
                 dp[i][j]=0;
                 continue;
               }
-              else {
-                  if(i==0 && j==0){
+              if(i==0 && j==0){
                   dp[i][j]=1;
                     }
-                  else{
+              else {
+                  
                       int up=0;
                       int left = 0;
                       if(i>0) 
@@ -24,7 +24,6 @@ public:
                       if(j>0)
                         left = dp[i][j-1];
                       dp[i][j] = up+left;
-                  }
               }
               
           }
