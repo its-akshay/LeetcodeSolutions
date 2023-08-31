0e7496baf8149e194eef5ab41464505a0e4e9258
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2023-08-31 16:00:08 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (67.20%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
+        int countWaysUtil(int i, int j, vector<vector<int> > &dp, vector<vector<int> > &maze) {
+            if(i>=0&&j>=0&&maze[i][j]==1){
+               return 0;
+            }
+            if (i == 0 && j == 0)
+                return 1;
+            if (i < 0 || j < 0 )
+                return 0;
+            if (dp[i][j] != -1) {return dp[i][j];}
 
-    int dp[101][101];
+            int up = countWaysUtil(i - 1, j, dp, maze);
+            int left = countWaysUtil(i, j - 1, dp, maze);
 
-    int util(vector<vector<int>>& grid, int m, int n, int i, int j)
-    {
-        if( i==m-1 && j==n-1)
-            return 1;
-        if( i>=m || j>=n )
-            return 0;
-        if(grid[i][j]==1)
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
+            return dp[i][j] = up + left;
+         }
 
-        return dp[i][j] = util(grid, m, n, i+1, j) + util(grid, m, n, i, j+1); 
-    }
 
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int m = grid.size();
-        int n = grid[0].size();
-        memset(dp, -1, sizeof(dp));
-        if(grid[m-1][n-1]==1)   return 0;
-        return util(grid, m, n, 0, 0);
+        int n= grid[0].size();
+        vector<vector<int> > dp(m, vector<int>(n, -1));
+        return countWaysUtil(m - 1, n - 1, dp, grid);
     }
+       
 };
