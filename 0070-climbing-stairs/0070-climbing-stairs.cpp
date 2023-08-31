b973bb4ef5cbf55d0df342c421859b6c866c161f
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2023-08-31 21:39:17 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (17.96%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        //we are here finding the distinct number of solutions actually
-        //so here by assigning the value as 1 we mean that 
-        if(n==0||n==1){
+    int sol(int n,vector<int>&dp){
+        if(n==0){
             return 1;
         }
-        dp[2] = 2;
-        dp[1] = 1;
-        
-        for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+        if(n==1){
+            return 1;
         }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        int one = sol(n-1,dp);
+        int two = sol(n-2,dp);
+        dp[n] = one+two;
         return dp[n];
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return sol(n,dp);
     }
 };
