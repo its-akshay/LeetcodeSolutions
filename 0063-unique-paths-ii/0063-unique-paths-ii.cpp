fbc73409741c53dabcb0938ac864562b3e3b383a
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2023-08-31 17:27:58 +0530

    Time: 5 ms (37.56%), Space: 7.8 MB (67.20%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
-        int countWaysUtil(int i, int j, vector<vector<int> > &dp, vector<vector<int> > &maze) {
-            if(i>=0&&j>=0&&maze[i][j]==1){
-               return 0;
-            }
-            if (i == 0 && j == 0)
-                return 1;
-            if (i < 0 || j < 0 )
-                return 0;
-            if (dp[i][j] != -1) {return dp[i][j];}
-
-            int up = countWaysUtil(i - 1, j, dp, maze);
-            int left = countWaysUtil(i, j - 1, dp, maze);
-
-            return dp[i][j] = up + left;
-         }
-
 
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int m = grid.size();
         int n= grid[0].size();
-        vector<vector<int> > dp(m, vector<int>(n, -1));
-        return countWaysUtil(m - 1, n - 1, dp, grid);
+         vector<vector<int> > dp(m, vector<int>(n, -1));
+
+      for(int i=0; i<m ;i++){
+          for(int j=0; j<n; j++){
+              if(grid[i][j]==1){
+                dp[i][j]=0;
+                continue;
+              }
+              else {
+                  if(i==0 && j==0){
+                  dp[i][j]=1;
+                    }
+                  else{
+                      int up=0;
+                      int left = 0;
+                      if(i>0) 
+                        up = dp[i-1][j];
+                      if(j>0)
+                        left = dp[i][j-1];
+                      dp[i][j] = up+left;
+                  }
+              }
+              
+          }
+      }
+      return dp[m-1][n-1];
     }
-       
 };
