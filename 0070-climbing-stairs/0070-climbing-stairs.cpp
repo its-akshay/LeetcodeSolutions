2eff1edf6317c1de0117846241092c3ebf66fcac
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2023-08-31 21:40:55 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (41.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
-    int sol(int n,vector<int>&dp){
-        if(n==0){
-            return 1;
-        }
-        if(n==1){
-            return 1;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        int one = sol(n-1,dp);
-        int two = sol(n-2,dp);
-        dp[n] = one+two;
-        return dp[n];
-    }
+
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return sol(n,dp);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+            
+        }
+        return dp[n];
     }
 };
