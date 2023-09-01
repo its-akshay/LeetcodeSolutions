7ad75dd8a942a382154e212c8402b123b424885c
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2023-09-01 16:18:52 +0530

    Time: 8 ms (42.94%), Space: 9 MB (23.01%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
+    int sol(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>&dp){
+      
+        if(dp[i][j]!=-1){return dp[i][j];}
+        if(i==triangle.size()-1){
+            return triangle[i][j];
+        }
+        int down = triangle[i][j]+sol(i+1,j,triangle,dp);
+        int diag = triangle[i][j]+sol(i+1,j+1,triangle,dp);
+        dp[i][j] = min(down,diag);
+        return dp[i][j];
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,0));
-        for(int j=0;j<n;j++){
-            dp[n-1][j] = triangle[n-1][j];
-        }
-        for(int i=n-2;i>=0;i--){
-            for(int j=i;j>=0;j--){
-                int down = triangle[i][j]+dp[i+1][j];
-                int diag = triangle[i][j]+dp[i+1][j+1];
-                dp[i][j] = min(down,diag);
-            }
-        }
-        return dp[0][0];
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        int x = sol(0,0,triangle,dp);
+        return x;
+
     }
 };
