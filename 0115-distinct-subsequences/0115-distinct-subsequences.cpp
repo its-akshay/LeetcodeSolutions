0066f6f5ab33441b2f42837aad6038dbe0f3c50a
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2023-09-22 13:06:55 +0530

    Time: 25 ms (80.53%), Space: 6.6 MB (86.14%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -4,24 +4,20 @@ int prime = 1e9+7;
     int numDistinct(string s1, string s2) {
        int n = s1.size();
         int m = s2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));    
-    for(int i=0;i<n+1;i++){
-        dp[i][0]=1;
-    }
-    for(int i=1;i<m+1;i++){
-        dp[0][i]=0;
-    }    
+        // vector<vector<int>> dp(n+1,vector<int>(m+1,0));    
+        vector<int>prev(m+1,0);
+        prev[0] = 1;   
     for(int i=1;i<n+1;i++){
-        for(int j=1;j<m+1;j++){
+        for(int j=m;j>=1;j--){
             
             if(s1[i-1]==s2[j-1])
-                dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%prime;
+                prev[j] = (prev[j-1] + prev[j])%prime;
             else
-                dp[i][j] = dp[i-1][j];
+                prev[j] = prev[j];
         }
     }    
     
-    return dp[n][m];
+    return prev[m];
         
     }
 };
