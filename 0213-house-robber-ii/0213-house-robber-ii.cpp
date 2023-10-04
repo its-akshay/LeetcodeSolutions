eee91d931c33941b9f713e10b1cfc200e1ea5505
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2023-10-04 13:20:08 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (16.69%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-     int robHelper(vector<int>& nums, int start, int end) {
-        int prev = 0;
-        int prev2 = 0;
-        
-        for (int i = start; i <= end; i++) {
-            int pick = nums[i] + prev2;
-            int notPick = prev;
-            int curr = max(pick, notPick);
-            prev2 = prev;
-            prev = curr;
-        }
-        
-        return prev;
-    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        
-        if (n == 1) {
+        vector<int>dp(n,-1);
+        if(n==1){
             return nums[0];
         }
+        int ExcludingLast = maxAm(0,n-2,nums,dp);
+        fill(dp.begin(),dp.end(),-1);
+        int Excludingfirst = maxAm(1,n-1,nums,dp);
         
-        return max(robHelper(nums, 0, n - 2), robHelper(nums, 1, n - 1));
+        return max(Excludingfirst,ExcludingLast);
+    }
+    int maxAm(int curr,int target,vector<int>&nums,vector<int>&dp){
+        if(curr==target){
+            return nums[curr];
+        }
+        if(curr>target){
+            return 0;
+        }
+        if(dp[curr]!=-1){
+            return dp[curr];
+        }
+        int rob = nums[curr] + maxAm(curr+2,target,nums,dp);
+        int nrob = maxAm(curr+1,target,nums,dp);
+        return dp[curr] = max(rob ,nrob);
+
     }
 };
