1d4c7e46d45449ffa6496879b4d5307853512be2
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2023-10-11 19:42:44 +0530

    Added solution - LeetHub

--- Inorder Traversal (Iterative) - GFG/inorder-traversal-iterative.cpp
+++ Inorder Traversal (Iterative) - GFG/inorder-traversal-iterative.cpp
@@ -98,25 +98,21 @@ class Solution {
 public:
     vector<int> inOrder(Node* root)
     {
-       stack<Node*>st;
-       Node*node = root;
-       vector<int>ans;
-       while(true){
-           if(node!=NULL){
-               st.push(node);
-               node = node->left;
-           }
-           else{
-               if(st.empty()==true){
-                   break;
-               }
-               node = st.top();
-               st.pop();
-               ans.push_back(node->data);
-               node = node->right;
-           }
-       }
-       return ans;
+        //code here
+        stack<Node*>st;
+        Node*curr = root;
+        vector<int>ans;
+        while(curr!=NULL ||st.empty()==false){
+            while(curr!=NULL){
+                st.push(curr);
+                curr = curr->left;
+            }
+            curr = st.top();
+            st.pop();
+            ans.push_back(curr->data);
+            curr = curr->right;
+        }
+        return ans;
     }
 };
 
