6138ab78939873a7d55494c34062a80eb743e87a
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2023-12-02 20:06:00 +0530

    Time: 167 ms (20.21%), Space: 53.4 MB (15.32%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int countCharacters(vector<string>& words, string chars) {
-        unordered_map<char, int> mp;
-        for (char ch : chars) {
-            mp[ch]++;
+       int ans = 0;
+        for (int i = 0; i < words.size(); i++) {
+            string word = words[i];
+            unordered_map<char, int> temp_mp;
+             for (int i = 0; i < chars.size(); i++) {
+            temp_mp[chars[i]]++;
         }
-
-        int ans = 0;
-        for (const string& word : words) {
-            unordered_map<char, int> temp_mp(mp);  // Make a copy of the original map for each word
-            bool valid = true;
-            for (char ch : word) {
-                if (temp_mp.find(ch) == temp_mp.end() || temp_mp[ch] == 0) {
-                    valid = false;
+            int x = 0;
+            for (int j = 0; j < word.size(); j++) {
+                if (temp_mp.find(word[j]) == temp_mp.end() || temp_mp[word[j]] == 0) {
                     break;
                 }
-                temp_mp[ch]--;
-            }
-            if (valid) {
-                ans += word.size();
+                temp_mp[word[j]]--;
+                x++;
+                if (x == word.size()) {
+                    ans += x;
+                }
             }
         }
-
         return ans;
     }
 };
